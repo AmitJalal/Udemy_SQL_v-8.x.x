82694aa5acaa94760e7e5b88696077b67d30ba69
AmitJalal <amitjalal53@gmail.com>
2022-11-02 22:57:40 +0530

    dbs and tables

---
+++ creating_dbs_&_tables/create-database.sql
@@ -0,0 +1,34 @@
+-- SELECT 9*9; 
+show databases;
+
+/*  Output
+        +--------------------+
+        | Database           |
+        +--------------------+
+        | amitdb             |
+        | information_schema |
+        | mysql              |
+        | performance_schema |
+        | sys                |
+        +--------------------+
+*/
+
+create database pet_shop;
+
+/*output 
+   Query OK, 1 row affected (0.00 sec)
+   
+    mysql> show databases;
+    +--------------------+
+    | Database           |
+    +--------------------+
+    | amitdb             |
+    | information_schema |
+    | mysql              |
+    | performance_schema |
+    | pet_shop           |
+    | sys                |
+    +--------------------+
+
+   6 rows in set (0.00 sec)
+ */
